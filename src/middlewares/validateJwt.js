0984f020ea66afa84ca8d23f8eb100e44fc35611
Sebas2411 <mastropiero2411@gmail.com>
2026-06-08 16:22:42 -0300

    se aplico autenticacion de rutas

--- src/middlewares/validateJwt.js
+++ src/middlewares/validateJwt.js
@@ -1,4 +1,20 @@
+import { verifyToken } from '../libs/jwt.js';
 
-function validateJWTToken(req, res, next) {}
+export function validateJwt(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  if (!authHeader) {
+    return res.status(401).json({ mensaje: 'Acceso denegado. No se proporcionó token.' });
+  }
 
-export{}
+  const token = authHeader.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({ mensaje: 'Acceso denegado. Formato de token inválido.' });
+  }
+
+  const decoded = verifyToken(token);
+  if (!decoded) {
+    return res.status(403).json({ mensaje: 'Token inválido o expirado.' });
+  }
+  req.user = decoded;
+  next();
+}
--- src/services/user.services.js
+++ src/services/user.services.js
@@ -1,5 +1,14 @@
-async function createUser() {
+import User from '../models/users.js';
 
+class UserService {
+  static async createUser(userData) {
+    const user = new User(userData);
+    return user.save();
+  }
+
+  static async findUserByEmail(email) {
+    return User.findOne({ email });
+  }
 }
 
-export { createUser };
+export default UserService;
