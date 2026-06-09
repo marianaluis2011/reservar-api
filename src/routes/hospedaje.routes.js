99c84f2be36ff98d9f05bd56fb6b6e2142e08cb7
Santiago Brizuela <brizuelasantiago433@gmail.com>
2026-06-09 13:59:09 -0300

    fix: proteger PUT de hospedaje con JWT y validar body en actualizar

--- src/routes/hospedaje.routes.js
+++ src/routes/hospedaje.routes.js
@@ -7,6 +7,6 @@ const router = Router();
 router.post('/', validateJwt, hospedajeController.registrar);
 router.get('/', hospedajeController.listarPublico);
 router.get('/:id', hospedajeController.obtenerDetalle);
-router.put('/:id', hospedajeController.actualizar);
+router.put('/:id', validateJwt, hospedajeController.actualizar);
 
 export default router;
