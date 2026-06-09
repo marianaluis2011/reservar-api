9a3b1a569037346da12a10c405e0799087cf295c
Sebas2411 <mastropiero2411@gmail.com>
2026-06-08 23:17:55 -0300

    se completa crud de reservas

--- src/routes/habitacion.routes.js
+++ src/routes/habitacion.routes.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import habitacionController from '../controllers/habitacionController.js';
 import { validateJwt } from '../middlewares/validateJwt.js';
+import { authorize } from '../middlewares/authorize.js';
 
 const router = Router();
 
-router.post('/', validateJwt, habitacionController.crear);
+router.post('/', validateJwt, authorize(['admin_hospedaje', 'super_admin']), habitacionController.crear);
 router.get('/hospedaje/:hospedajeId', habitacionController.listarPorHospedaje);
 router.get('/:id', habitacionController.obtenerDetalle);
-router.put('/:id', habitacionController.actualizar);
-router.delete('/:id', habitacionController.eliminar);
+router.put('/:id', validateJwt, authorize(['admin_hospedaje', 'super_admin']), habitacionController.actualizar);
+router.delete('/:id', validateJwt, authorize(['admin_hospedaje', 'super_admin']), habitacionController.eliminar);
 
 export default router;
