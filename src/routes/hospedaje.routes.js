2b37d5f183b891c826af2bdf7e1be05492c3aa7c
Sebas2411 <mastropiero2411@gmail.com>
2026-06-11 21:30:47 -0300

    se configura ruta para arhivos y se actualiza controlador para urls

--- src/routes/hospedaje.routes.js
+++ src/routes/hospedaje.routes.js
@@ -4,15 +4,25 @@ import { validateJwt } from '../middlewares/validateJwt.js';
 import { authorize } from '../middlewares/authorize.js';
 import { createAccommodationValidator, updateAccommodationValidator, accommodationIdParamValidator } from '../validators/hospedajeValidation.js';
 import { validateResult } from '../middlewares/validateResult.js';
+import { upload } from '../config/cloudinary.js';
 
 const router = Router();
 
+// Rutas públicas
 // Rutas públicas
 router.get('/', hospedajeController.listarPublico);
 router.get('/:id', accommodationIdParamValidator, validateResult, hospedajeController.obtenerDetalle);
 
 // Rutas protegidas
-router.post('/', validateJwt, authorize(['super_admin']), createAccommodationValidator, validateResult, hospedajeController.registrar);
+router.post(
+  '/', 
+  validateJwt, 
+  authorize(['super_admin']), 
+  upload.fields([{ name: 'imagenPrincipal', maxCount: 1 }, { name: 'galeria', maxCount: 5 }]),
+  createAccommodationValidator, 
+  validateResult, 
+  hospedajeController.registrar
+);
 router.put('/:id', validateJwt, authorize(['admin_hospedaje', 'super_admin']), accommodationIdParamValidator, updateAccommodationValidator, validateResult, hospedajeController.actualizar);
 router.delete('/:id', validateJwt, authorize(['admin_hospedaje', 'super_admin']), accommodationIdParamValidator, validateResult, hospedajeController.eliminar);
 
